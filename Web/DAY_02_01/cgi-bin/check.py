e1171b391202496043482ab1d63381dcd5574365
ParkHeeJin00 <mybae78@gmail.com>
2024-04-09 17:40:53 +0900

    html_css

---
+++ Web/DAY_02_01/cgi-bin/check.py
@@ -0,0 +1,6 @@
+# HTML Header
+print('Content-Type : text/html')
+print()  # 무조건 한줄 띄어야 함
+
+# HTML Body
+print('hello CGI')
---
+++ Web/DAY_02_01/cgi-bin/index_01.py
@@ -0,0 +1,37 @@
+# 모듈 로딩
+import cgi, sys, codecs, os, datetime
+import cgitb
+cgitb.enable()  # Error 확인
+
+# Web 인코딩 설정
+sys.stdout = codecs.getwriter(encoding='utf-8')(sys.stdout.detach())
+
+# 요청 처리 및 브라우징
+# Client 요청 데이터 즉, Form 데이터 저장 인스턴스
+form = cgi.FieldStorage()
+
+
+# Web 브라우저 화면 출력 코드
+
+filepath = './HTML/test.html'
+
+def print_html(filename, data = ''):
+    # HTML 파일 읽기 => body 문자열
+    with open(filename, 'r', encoding='utf-8') as f:
+    
+        # HTML Header
+        print('Content-Type : text/html; charset = utf-8')
+        print()  # 무조건 한줄 띄어야 함
+
+        # HTML Body
+        body = f.read().format(data)
+        print(body)
+
+# 데이터 추출
+if 'data' in form and 'no' in form:
+    result = form.getvalue('data') + '-' + form.getvalue('no')
+else:
+    result = "No Data"
+
+# 브라우징 : 함수 실행
+print_html(filepath, result)
