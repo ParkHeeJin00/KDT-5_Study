be091e4d0a13ca6234286d03ff88c23a7463bcdb
ParkHeeJin00 <mybae78@gmail.com>
2024-04-19 10:21:15 +0900

    inputdata

--- Web/Flask_D03/YouWeb/views/main_views.py
+++ Web/Flask_D03/YouWeb/views/main_views.py
@@ -1,5 +1,5 @@
-## 모듈로딩
-from flask import Blueprint, render_template
+    ## 모듈로딩
+from flask import Blueprint, render_template, request, redirect, url_for
 from YouWeb.models import Question
 
 # BP 인스턴스 생성
@@ -15,3 +15,16 @@ def index():
     question_list = Question.query.order_by(Question.create_date.desc())
 
     return render_template('question_list.html', question_list = question_list)
+
+@bp.route('/question/input/')
+def input_question():
+    # 질문 입력 페이지 출력
+    return render_template('question_input.html')
+
+@bp.route('/question/input/finish/', methods=['POST'])
+def input_finish():
+    # 질문 입력 페이지 출력
+    req_dict = request.form.to_dict()
+    title = req_dict.get('title')
+    content = req_dict.get('content')       
+    return render_template('question_input_finish.html', title = title, content = content)
